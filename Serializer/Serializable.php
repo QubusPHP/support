b5121b86f70e2ef2738b00693ac5dd313551ed11
Joshua Parker <joshua@joshuaparker.dev>
2023-08-24 21:46:52 -0600

    Removed since tag from class header.

--- Serializer/Serializable.php
+++ Serializer/Serializable.php
@@ -7,7 +7,6 @@
  * @copyright  2022
  * @author     Joshua Parker <joshua@joshuaparker.dev>
  * @license    https://opensource.org/licenses/mit-license.php MIT License
- * @since      2.2.2
  */
 
 declare(strict_types=1);
