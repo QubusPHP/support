507f27c4722277f52798b43af41cfce12d007ad8
Joshua Parker <joshua@joshuaparker.dev>
2025-02-16 10:57:46 -0800

    Fixed bug.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Collection/ValueExtractorAware.php
+++ Collection/ValueExtractorAware.php
@@ -24,19 +24,32 @@ trait ValueExtractorAware
      * Extracts the value of the given property or method from the object.
      *
      * @param mixed $object The object to extract the value from.
-     * @param string $propertyOrMethod The property or method for which the
-     *                                 value should be extracted.
+     * @param string|null $propertyOrMethod The property or method for which the
+     *                                      value should be extracted.
      * @return mixed the value extracted from the specified property or method.
      * @throws ValueExtractionException if the method or property is not defined.
      */
-    protected function extractValue(mixed $object, string $propertyOrMethod): mixed
+    protected function extractValue(mixed $object, ?string $propertyOrMethod = null): mixed
     {
-        if (!is_object(value: $object)) {
+        if ($propertyOrMethod === null) {
+            return $object;
+        }
+
+        if (!is_object(value: $object) && !is_array($object)) {
             throw new ValueExtractionException(message: 'Unable to extract a value from a non-object.');
         }
 
+        if (is_array($object)) {
+            return $object[$propertyOrMethod] ?? throw new InvalidPropertyOrMethodException(
+                sprintf(
+                    'Key or index "%s" not found in collection elements',
+                    $propertyOrMethod,
+                )
+            );
+        }
+
         if (property_exists($object, $propertyOrMethod)) {
-            return $object->$propertyOrMethod;
+            return $object->{$propertyOrMethod};
         }
 
         if (method_exists($object, $propertyOrMethod)) {
